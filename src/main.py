ce950df59b7590c0b97aa5e9b266dd4364462911
aapolipponen <aapo.lipponen@gmail.com>
2023-07-22 19:08:15 +0300

    Revert "Full orbit drawing system"
    This reverts commit c5b1838ad7ae769b6464b22ba64abe51da231861.
    
    Wrong branch, reverting changes.

--- src/main.py
+++ src/main.py
@@ -1,5 +1,4 @@
-from planet import sun, earth, bodies
-from constants import day
+from planet import SpaceObject, BODIES, PLANETS, sun, earth, jupiter, mars
 import pygame
 from simulation import run_simulation
 from display import draw_objects
@@ -8,12 +7,12 @@ focus_object = sun
 
 FULL_ORBITS = True
 
-star_size_multiplier = 1
-planet_size_multiplier = 1
-moon_size_multiplier = 1
+SUN_SCALE_MULTIPLIER = 1
+PLANET_SCALE_MULTIPLIER = 1
+MOON_SCALE_MULTIPLIER = 1
 
 # Define the initial timescale value in seconds
-timescale_seconds = day
+timescale_seconds = 60 * 60
 
 # Calculate scaling factors for size and distance
 SCALE_DIST = 2e-10  # Scale down distance by 1e9 for display (1 meter = 1e-9 pixels)
@@ -21,8 +20,8 @@ SCALE_DIST = 2e-10  # Scale down distance by 1e9 for display (1 meter = 1e-9 pix
 # Main simulation loop
 running = True
 while running:
-    for body in bodies:
+    for SpaceObject in BODIES:
         run_simulation(timescale_seconds)
-    draw_objects(focus_object, timescale_seconds, SCALE_DIST, star_size_multiplier, planet_size_multiplier, moon_size_multiplier, FULL_ORBITS)
+    draw_objects(focus_object, timescale_seconds, SCALE_DIST, SUN_SCALE_MULTIPLIER, PLANET_SCALE_MULTIPLIER, MOON_SCALE_MULTIPLIER, FULL_ORBITS)
 
 pygame.quit()
