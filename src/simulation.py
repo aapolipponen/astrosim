7e1171e1d23a70b32fc65620fd26b188bb344994
aapolipponen <aapo.lipponen@gmail.com>
2023-08-07 15:20:02 +0300

    Additions to orbital system

--- src/simulation.py
+++ src/simulation.py
@@ -1,8 +1,8 @@
 from constants import G
-from planet import bodies, sun
+from planet import bodies
 import numpy as np
 
-def calculate_net_force(target_body):
+def calculate_net_force(target_body, use_post_newtonian=True):
     net_force = np.array([0.0, 0.0, 0.0])
     for body in bodies:
         if body != target_body:
@@ -10,80 +10,104 @@ def calculate_net_force(target_body):
             r_mag = np.linalg.norm(r)
             force_mag = G * target_body.mass * body.mass / r_mag**2
             force = force_mag * r / r_mag
+
+            if use_post_newtonian:
+                # First post-Newtonian correction
+                c_squared = (3e8)**2  # Speed of light, m/s
+                v_squared = np.dot(body.vel, body.vel)
+                v_dot_r = np.dot(body.vel, r)
+                force_correction = G * target_body.mass * body.mass / (r_mag**2 * c_squared) * \
+                                   ((4*G*body.mass/r_mag - v_squared)*r + 4*v_dot_r*body.vel)
+                force += force_correction
+
             net_force += force
     return net_force
 
+def derivatives(body):
+    net_force = calculate_net_force(body)
+    acc = net_force / body.mass  # Acceleration = net force / mass
+    return body.vel, acc
+
 def euler_integration(body, timescale_seconds):
-    pos_derivative, vel_derivative = derivatives(body, body.pos, body.vel)
-    body.pos += pos_derivative * timescale_seconds
-    body.vel += vel_derivative * timescale_seconds
-
-def orbital_elements_integration(body, focus_object, timescale_seconds):
-    # Retrieve the orbital elements
-    a, e, i, omega, w, M = get_orbital_elements(body, focus_object)
-
-    # Update mean anomaly based on mean motion
-    n = np.sqrt(G * (body.mass + focus_object.mass) / a**3)
-    M += n * timescale_seconds
-
-    # Solve Kepler's equation for eccentric anomaly (E) using M and e
-    E = solve_keplers_equation(M, e)
-
-    # Calculate true anomaly (f)
-    f = 2 * np.arctan2(np.sqrt(1 + e) * np.tan(E / 2), np.sqrt(1 - e))
-
-    # Calculate r, the distance from the focus object
-    r = a * (1 - e * np.cos(E))
-
-    # Update position and velocity using f, r, and other orbital elements
-    update_body_state_from_orbital_elements(body, focus_object, r, f, w, omega, i)
-
-    # You can also update the orbital elements if needed for the next step
-    update_orbital_elements(body, focus_object)
+    vel_derivative, acc_derivative = derivatives(body)
+    body.pos += vel_derivative * timescale_seconds
+    body.vel += acc_derivative * timescale_seconds
 
 def midpoint_integration(body, timescale_seconds):
-    k1_pos, k1_vel = derivatives(body, body.pos, body.vel)
-    k1_pos *= timescale_seconds
-    k1_vel *= timescale_seconds
 
-    k2_pos, k2_vel = derivatives(body, body.pos + 0.5 * k1_pos, body.vel + 0.5 * k1_vel)
-    k2_pos *= timescale_seconds
-    k2_vel *= timescale_seconds
+    k1_vel, k1_acc = derivatives(body)
+    k1_pos = k1_vel * timescale_seconds
+    k1_acc *= timescale_seconds
 
+    # temporary update to midpoint state
+    body.pos += 0.5 * k1_pos
+    body.vel += 0.5 * k1_acc
+
+    k2_vel, k2_acc = derivatives(body)
+    k2_pos = k2_vel * timescale_seconds
+    k2_acc *= timescale_seconds
+
+    # Roll back the temporary update
+    body.pos -= 0.5 * k1_pos
+    body.vel -= 0.5 * k1_acc
+
+    # Do the actual update
     body.pos += k2_pos
-    body.vel += k2_vel
+    body.vel += k2_acc
 
 def heun_integration(body, timescale_seconds):
-    k1_pos, k1_vel = derivatives(body, body.pos, body.vel)
-    k1_pos *= timescale_seconds
-    k1_vel *= timescale_seconds
+    k1_vel, k1_acc = derivatives(body)
+    k1_pos = k1_vel * timescale_seconds
+    k1_acc *= timescale_seconds
 
-    k2_pos, k2_vel = derivatives(body, body.pos + k1_pos, body.vel + k1_vel)
-    k2_pos *= timescale_seconds
-    k2_vel *= timescale_seconds
+    # temporary update to predict the end state
+    body.pos += k1_pos
+    body.vel += k1_acc
 
+    k2_vel, k2_acc = derivatives(body)
+    k2_pos = k2_vel * timescale_seconds
+    k2_acc *= timescale_seconds
+
+    # Roll back the temporary update
+    body.pos -= k1_pos
+    body.vel -= k1_acc
+
+    # Do the actual update with the average of the initial and predicted end state
     body.pos += 0.5 * (k1_pos + k2_pos)
-    body.vel += 0.5 * (k1_vel + k2_vel)
+    body.vel += 0.5 * (k1_acc + k2_acc)
 
 def rk4_integration(body, timescale_seconds):
-    k1_pos, k1_vel = derivatives(body, body.pos, body.vel)
-    k1_pos *= timescale_seconds
-    k1_vel *= timescale_seconds
+    pos_initial = body.pos.copy()
+    vel_initial = body.vel.copy()
 
-    k2_pos, k2_vel = derivatives(body, body.pos + 0.5 * k1_pos, body.vel + 0.5 * k1_vel)
-    k2_pos *= timescale_seconds
-    k2_vel *= timescale_seconds
+    k1_vel, k1_acc = derivatives(body)
+    k1_pos = k1_vel * timescale_seconds
+    k1_acc *= timescale_seconds
 
-    k3_pos, k3_vel = derivatives(body, body.pos + 0.5 * k2_pos, body.vel + 0.5 * k2_vel)
-    k3_pos *= timescale_seconds
-    k3_vel *= timescale_seconds
+    body.pos = pos_initial + 0.5 * k1_pos
+    body.vel = vel_initial + 0.5 * k1_acc
+    k2_vel, k2_acc = derivatives(body)
+    k2_pos = k2_vel * timescale_seconds
+    k2_acc *= timescale_seconds
 
-    k4_pos, k4_vel = derivatives(body, body.pos + k3_pos, body.vel + k3_vel)
-    k4_pos *= timescale_seconds
-    k4_vel *= timescale_seconds
+    body.pos = pos_initial + 0.5 * k2_pos
+    body.vel = vel_initial + 0.5 * k2_acc
+    k3_vel, k3_acc = derivatives(body)
+    k3_pos = k3_vel * timescale_seconds
+    k3_acc *= timescale_seconds
+
+    body.pos = pos_initial + k3_pos
+    body.vel = vel_initial + k3_acc
+    k4_vel, k4_acc = derivatives(body)
+    k4_pos = k4_vel * timescale_seconds
+    k4_acc *= timescale_seconds
+
+    # return body's position and velocity to the initial values before applying the final update
+    body.pos = pos_initial
+    body.vel = vel_initial
 
     body.pos += (k1_pos + 2 * k2_pos + 2 * k3_pos + k4_pos) / 6
-    body.vel += (k1_vel + 2 * k2_vel + 2 * k3_vel + k4_vel) / 6
+    body.vel += (k1_acc + 2 * k2_acc + 2 * k3_acc + k4_acc) / 6
 
 def verlet_integration(body, timescale_seconds):
     # Compute current acceleration
@@ -123,16 +147,16 @@ def get_integrator(method):
         'rk4': rk4_integration, # Fourth-order Runge-Kutta method. A well-balanced choice for many problems, offering good accuracy and stability. More computationally expensive than Euler but widely used in various fields.
         'verlet': verlet_integration, # Commonly used in molecular dynamics simulations and other physical simulations. It conserves energy over long simulations but may be less accurate in terms of position.
         'leapfrog': leapfrog_integration, # A time-reversible method often used in gravitational simulations. It conserves momentum and is more accurate than simple methods like Euler for oscillatory problems.
-        'orbital_elements': orbital_elements_integration # Designed for integrating celestial bodies following Kepler's laws. It can be highly accurate for such applications, relying on specific orbital parameters like semi-major axis, eccentricity, etc.
+        #'orbital_elements': orbital_elements_integration # Designed for integrating celestial bodies following Kepler's laws. It can be highly accurate for such applications, relying on specific orbital parameters like semi-major axis, eccentricity, etc.
     }
-    return integrators.get(method, leapfrog_integration) # Default to Leapfrog
+    return integrators.get(method)
 
-def run_simulation(timescale_seconds, method='leapfrog'):
+def run_simulation(timescale_seconds, method):
     integrator = get_integrator(method)
     for body in bodies:
-        integrator(body, timescale_seconds)
         if body.parent is not None:
             calculate_orbital_parameters(body, body.parent)
+        integrator(body, timescale_seconds)
 
 def calculate_orbital_position(body, focus_object):
     # Get the position of the body relative to the focus_object
@@ -155,6 +179,12 @@ def calculate_energy_and_momentum(body, focus_object):
     body.L = np.linalg.norm(np.cross(body.relative_pos, body.relative_vel))
 
 def calculate_semi_major_axis(body, focus_object):
+    eccentricity_ratio = (1 - body.eccentricity) / (1 + body.eccentricity)
+    if eccentricity_ratio >= 0:
+        E = 2 * np.arctan(np.sqrt(eccentricity_ratio) * np.tan(0.5 * body.true_anomaly))
+    else:
+        # handle the error, for example by setting E to NaN or raising an exception
+        E = np.nan
     body.semi_major_axis = -G * focus_object.mass / (2 * body.E)
 
 def calculate_eccentricity(body, focus_object):
@@ -163,25 +193,80 @@ def calculate_eccentricity(body, focus_object):
 
     # The distance between the body and the focus object
     r = np.linalg.norm(body.pos - focus_object.pos)
-    
+
     # The speed of the body
     v = np.linalg.norm(body.vel)
-    
+
     # The specific relative angular momentum
     h = np.linalg.norm(np.cross(body.pos - focus_object.pos, body.vel))
-    
+
     # The specific orbital energy
     epsilon = v**2 / 2 - mu / r
-    
+
     # The eccentricity of the orbit
     body.eccentricity = np.sqrt(1 + 2 * epsilon * h**2 / mu**2)
 
 def calculate_semi_minor_axis(body):
     body.semi_minor_axis = body.semi_major_axis * np.sqrt(1 - body.eccentricity**2)
 
+def calculate_inclination(body):
+    body.inclination = np.arccos(np.dot(body.relative_pos, np.array([0, 0, 1])) / np.linalg.norm(body.relative_pos))
+
+def calculate_argument_of_periapsis(body):
+    n = np.cross(np.array([0, 0, 1]), body.relative_pos)
+    e = body.eccentricity
+    body.argument_of_periapsis = np.arccos(np.dot(n, body.relative_pos) / (np.linalg.norm(n) * np.linalg.norm(body.relative_pos)))
+    if body.relative_pos[2] < 0:
+        body.argument_of_periapsis = 2 * np.pi - body.argument_of_periapsis
+
+def calculate_longitude_of_ascending_node(body):
+    # The specific relative angular momentum
+    h = np.cross(body.relative_pos, body.relative_vel)
+
+    # The x and y components of h
+    h_x, h_y = h[0], h[1]
+
+    # Calculate the Longitude of the Ascending Node
+    Ω = np.arctan2(h_y, h_x)
+
+    # Convert to degrees and correct for negative angles
+    Ω = np.degrees(Ω)
+    body.longitude_of_ascending_node = Ω
+
+def calculate_mean_anomaly(body):
+    E = 2 * np.arctan(np.sqrt((1 - body.eccentricity) / (1 + body.eccentricity)) * np.tan(0.5 * body.true_anomaly))
+    body.mean_anomaly = E - body.eccentricity * np.sin(E)
+
+def calculate_true_anomaly(body, focus_object):
+    r = body.pos - focus_object.pos
+    v = body.vel - focus_object.vel
+    h = np.cross(r, v)
+    n = np.cross([0, 0, 1], h)
+
+    norm_n = np.linalg.norm(n)
+    norm_r = np.linalg.norm(r)
+
+    epsilon = 1e-10  # Small tolerance value
+    if norm_n < epsilon or norm_r < epsilon:
+        theta = 0
+    else:
+        cos_theta = np.dot(n, r) / (norm_n * norm_r)
+        cos_theta = np.clip(cos_theta, -1, 1)  # Ensure the value is within the interval [-1,1]
+        theta = np.arccos(cos_theta)
+
+    if np.dot(r, v) < 0:
+        theta = 2 * np.pi - theta
+
+    body.true_anomaly = theta
+
 def calculate_orbital_parameters(body, focus_object):
     calculate_relative_vectors(body, focus_object)
     calculate_energy_and_momentum(body, focus_object)
     calculate_semi_major_axis(body, focus_object)
-    calculate_eccentricity(body, focus_object)
     calculate_semi_minor_axis(body)
+    calculate_eccentricity(body, focus_object)
+    calculate_inclination(body)
+    calculate_longitude_of_ascending_node(body)
+    calculate_argument_of_periapsis(body)
+    calculate_mean_anomaly(body)
+    calculate_true_anomaly(body, focus_object)
